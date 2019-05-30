30bda92281448f4510b99b91a3121a6e9bb3eab7
Iván Hoffmann <digital.h@gmail.com>
2019-05-30 17:35:15 -0300

    Agregando campos al formulario

--- presentes/models/casos.py
+++ presentes/models/casos.py
@@ -17,11 +17,15 @@ class Caso(models.Model):
         (SI, 'Si'),
         (NO, 'No')
     )
+    OPCIONES_PUBLICA_PRIVADA = (
+        ('PU', 'Pública'),
+        ('PR', 'Privada')
+    )
     #Datos de la víctima
     nombre = models.CharField(max_length=200, default="", help_text="")
     apellido = models.CharField(max_length=200, default="", help_text="")
     lugar_de_nacimiento = models.CharField(max_length=255, default="", blank=True, null=True, help_text="")
-    edad = models.CharField(max_length=3, default="", help_text="")
+    edad = models.CharField(max_length=3, default="", blank=True, null=True, help_text="")
 
 
     #Datos del hecho
@@ -35,10 +39,30 @@ class Caso(models.Model):
     etiquetas = models.ManyToManyField('Etiqueta', related_name="casos", default=None, blank=True)
 
     #Si es muerte
-    causa_de_la_muerte = models.CharField(max_length=200, default="", blank=True, help_text="")
+    #Travesticidio y transfemicidio
+    causa_de_la_muerte = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    tenia_obra_social = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    obra_social = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    tiene_acceso_a_prestaciones_de_salud = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    prestaciones_de_salud = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    #Travesticidio y Travesticidio social
+    ocupacion = models.CharField(max_length=200, default="", blank=True, help_text="")
+    estudios_cursados = models.CharField(max_length=200, default="", blank=True, help_text="")
+    estaba_en_situacion_de_calle = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    donde_vivia = models.CharField(max_length=200, default="", blank=True, help_text="")
+
+    #Situación de encierro
+    # desde_cuando_estaba_detenida
+    tenia_prision_preventiva = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    titulo_de_la_causa_en_la_justicia = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    nombre_del_penal = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    localidad_del_penal = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    provincia_del_penal = models.ForeignKey('Provincia', related_name="casos_penal", default=None, blank=True, null=True, on_delete=models.CASCADE)
+
 
     #Es migrante
     es_migrante =  models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+
     pais_de_origen = models.CharField(max_length=200, default="", blank=True, help_text="")
     anio_de_llegada = models.CharField(max_length=4, default="", blank=True, help_text="")
 
@@ -46,8 +70,33 @@ class Caso(models.Model):
     hay_denuncia = models.CharField(max_length=2, choices=OPCIONES_SI_NO_NOSABE, default=None, null=True, blank=True)
     fecha_de_denuncia = models.DateField(default=datetime.date.today, null=True, blank=True)
     ante_quien_se_hizo_la_denuncia = models.CharField(max_length=200, default="", blank=True, help_text="")
-    por_que_no_denuncio = models.CharField(max_length=200, default="", blank=True, help_text="")
+    por_que_no_denuncio = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
     la_denuncia_reconoce_genero = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+
+    #Causa judicial
+    hay_causa_judicial = models.CharField(max_length=2, choices=OPCIONES_SI_NO_NOSABE, default=None, null=True, blank=True)
+    cj_titulo_de_la_causa = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    cj_numero_de_la_causa = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    cj_anio_de_inicio = models.CharField(max_length=4, default="", null=True, blank=True, help_text="")
+    cj_donde_se_tramita = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    cj_instancia = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    cj_respetaron_nombre_de_ig = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    cj_organismos_publicos = models.TextField(default="", null=True, blank=True, help_text="")
+    cj_organizaciones = models.ManyToManyField('Organizacion', related_name="casos", default=None, blank=True)
+    cj_cuenta_con_defensa = models.CharField(max_length=2, choices=OPCIONES_PUBLICA_PRIVADA, default=None, null=True, blank=True)
+    cj_hay_informe_forense = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+
+    #Violencia institucional
+    hubo_violencia_institucional = models.CharField(max_length=2, choices=OPCIONES_SI_NO, default=None, null=True, blank=True)
+    violencia_institucion_nombre = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    violencia_institucion_localidad = models.CharField(max_length=200, default="", null=True, blank=True, help_text="")
+    violencia_institucion_provincia = models.ForeignKey('Provincia', related_name="casos_violencia_institucional", default=None, blank=True, null=True, on_delete=models.CASCADE)
+
+    observaciones = models.TextField(default="", blank=True, help_text="")
+
+    #La informacion del formulario fue brindada por
+    nombre_de_quien_brindo_informacion = models.CharField(max_length=200, default="", blank=True, help_text="")
+    telefono_de_quien_brindo_informacion = models.CharField(max_length=200, default="", blank=True, help_text="")
 
 
 
--- presentes/serializers/casos.py
+++ presentes/serializers/casos.py
@@ -3,6 +3,7 @@ from presentes.models.casos import Caso
 from presentes.models.provincias import Provincia
 from presentes.models.categorias import Categoria
 from presentes.models.etiquetas import Etiqueta
+from presentes.models.organizaciones import Organizacion
 from presentes.models.estados_de_caso import EstadoDeCaso
 from rest_framework_json_api.relations import ResourceRelatedField
 
@@ -10,8 +11,11 @@ from rest_framework_json_api.relations import ResourceRelatedField
 class CasoSerializer(serializers.ModelSerializer):
 
     provincia = ResourceRelatedField(queryset=Provincia.objects, many=False, read_only=False)
+    provincia_del_penal = ResourceRelatedField(many=False, read_only=True)
+    violencia_institucion_provincia = ResourceRelatedField(many=False, read_only=True)
     categoria = ResourceRelatedField(queryset=Categoria.objects, many=False, read_only=False)
     etiquetas = ResourceRelatedField(many=True, read_only=True)
+    cj_organizaciones = ResourceRelatedField(many=True, read_only=True)
     estado_de_publicacion = ResourceRelatedField(queryset=EstadoDeCaso.objects, many=False, read_only=False)
 
     class Meta:
@@ -24,15 +28,27 @@ class CasoSerializer(serializers.ModelSerializer):
             'apellido',
             'lugar_de_nacimiento',
             'edad',
+            'fecha_del_hecho',
             'localidad',
             'provincia',
-            'fecha_del_hecho',
             'latitud',
             'longitud',
             'categoria',
             'etiquetas',
-            'estado_de_publicacion',
             'causa_de_la_muerte',
+            'tenia_obra_social',
+            'obra_social',
+            'tiene_acceso_a_prestaciones_de_salud',
+            'prestaciones_de_salud',
+            'ocupacion',
+            'estudios_cursados',
+            'estaba_en_situacion_de_calle',
+            'donde_vivia',
+            'tenia_prision_preventiva',
+            'titulo_de_la_causa_en_la_justicia',
+            'nombre_del_penal',
+            'localidad_del_penal',
+            'provincia_del_penal',
             'es_migrante',
             'pais_de_origen',
             'anio_de_llegada',
@@ -40,14 +56,36 @@ class CasoSerializer(serializers.ModelSerializer):
             'fecha_de_denuncia',
             'ante_quien_se_hizo_la_denuncia',
             'por_que_no_denuncio',
-            'la_denuncia_reconoce_genero'
+            'la_denuncia_reconoce_genero',
+            'hay_causa_judicial',
+            'cj_titulo_de_la_causa',
+            'cj_numero_de_la_causa',
+            'cj_anio_de_inicio',
+            'cj_donde_se_tramita',
+            'cj_instancia',
+            'cj_respetaron_nombre_de_ig',
+            'cj_organismos_publicos',
+            'cj_organizaciones',
+            'cj_cuenta_con_defensa',
+            'cj_hay_informe_forense',
+            'hubo_violencia_institucional',
+            'violencia_institucion_nombre',
+            'violencia_institucion_localidad',
+            'violencia_institucion_provincia',
+            'observaciones',
+            'nombre_de_quien_brindo_informacion',
+            'telefono_de_quien_brindo_informacion',
+            'estado_de_publicacion',
         ]
 
     included_serializers = {
         'etiquetas': 'presentes.serializers.etiquetas.EtiquetaSerializer',
         'provincia': 'presentes.serializers.provincias.ProvinciaSerializer',
+        'provincia_del_penal': 'presentes.serializers.provincias.ProvinciaSerializer',
+        'violencia_institucion_provincia': 'presentes.serializers.provincias.ProvinciaSerializer',
         'categoria': 'presentes.serializers.categorias.CategoriaSerializer',
-        'estado_de_publicacion': 'presentes.serializers.estados_de_caso.EstadoDeCasoSerializer'
+        'estado_de_publicacion': 'presentes.serializers.estados_de_caso.EstadoDeCasoSerializer',
+        'cj_organizaciones': 'presentes.serializers.organizaciones.OrganizacionSerializer'
     }
 
     class JSONAPIMeta:
@@ -55,5 +93,8 @@ class CasoSerializer(serializers.ModelSerializer):
             'categoria',
             'etiquetas',
             'provincia',
+            'provincia_del_penal',
+            'violencia_institucion_provincia',
+            'cj_organizaciones'
             'estado_de_publicacion'
         ]
