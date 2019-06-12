aa77c3e7d37dacdd3bdb13821864140619a41cbe
Iván Hoffmann <digital.h@gmail.com>
2019-06-12 20:15:42 -0300

    Fixes en view y serializador de organizaciones

--- presentes/serializers/organizaciones.py
+++ presentes/serializers/organizaciones.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from presentes.models.organizaciones import Organizacion
+from presentes.models.provincias import Provincia
 from rest_framework_json_api.relations import ResourceRelatedField
 
 
 class OrganizacionSerializer(serializers.ModelSerializer):
 
-    provincia = ResourceRelatedField(many=False, read_only=True)
+    provincia = ResourceRelatedField(queryset=Provincia.objects, many=False, read_only=False)
 
     class Meta:
         model = Organizacion
--- presentes/views/organizaciones.py
+++ presentes/views/organizaciones.py
@@ -6,6 +6,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework import permissions
 
+from presentes.models.provincias import Provincia
+
 class OrganizacionViewSet(viewsets.ModelViewSet):
     queryset = Organizacion.objects.all()
     resource_name = 'organizaciones'
@@ -20,6 +22,8 @@ class OrganizacionViewSet(viewsets.ModelViewSet):
         queryset = self.queryset
         query = self.request.query_params.get('query', '')
         nombre = self.request.query_params.get('nombre', '')
+        localidad = self.request.query_params.get('localidad', None)
+        provincia = self.request.query_params.get('provincia', '')
         ordenamiento = self.request.query_params.get('ordenamiento', '')
 
         queryset = queryset.filter(nombre__icontains=query)
@@ -29,5 +33,12 @@ class OrganizacionViewSet(viewsets.ModelViewSet):
 
         if nombre:
             queryset = queryset.filter(nombre__icontains=nombre)
+
+        if localidad:
+            queryset = queryset.filter(localidad__icontains=localidad)
+
+        if provincia:
+            provinciaComoObjeto = Provincia.objects.get(nombre=provincia)
+            queryset = queryset.filter(provincia__in=[provinciaComoObjeto.id])
 
         return queryset
